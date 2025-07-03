bcf605b7f18db0738a50fbb1ffd0c5510224baef
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2025-07-03 16:10:31 +0300

    Section 6: Wire-up Dependencies Between Modules (DI Management of Modules)

--- src/Bootstrapper/Api/Program.cs
+++ src/Bootstrapper/Api/Program.cs
@@ -1,9 +1,15 @@
 var builder = WebApplication.CreateBuilder(args);
 
-
+builder.Services
+        .AddCatalogModule(builder.Configuration)
+        .AddBasketModule(builder.Configuration)
+        .AddOrderingModule(builder.Configuration);
 
 var app = builder.Build();
 
+app.UseCatalogModule()
+   .UseBasketModule()
+   .UseOrderingModule();
 
 
 app.Run();
---
+++ src/Modules/Catalog/Catalog/CatalogModule.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog
+{
+    public static class CatalogModule
+    {
+        public static IServiceCollection AddCatalogModule(this IServiceCollection service,IConfiguration configuration) {
+            return service;
+        }
+        public static IApplicationBuilder UseCatalogModule(this IApplicationBuilder app)
+        {
+            return app;
+        }
+    }
+}
