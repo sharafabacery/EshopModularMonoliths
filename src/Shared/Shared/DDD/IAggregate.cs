154e914c0aa10541e766117cfab1abbce04bd48f
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2025-07-05 13:12:25 +0300

    Section 8: Develop Shared Domain for Tactical DDD Abstraction (Entity, Aggreagate, Events)

---
+++ src/Shared/Shared/DDD/IAggregate.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.DDD
+{
+    public interface IAggregate<T> : IAggregate, IEntity<T>
+    {
+
+    }
+    public interface IAggregate:IEntity
+    {
+        IReadOnlyList<IDomainEvent> DomainEvents {  get; }
+        IDomainEvent[] clearDomainEvents();
+    }
+}
---
+++ src/Shared/Shared/DDD/IDomainEvent.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Transactions;
+using MediatR;
+
+namespace Shared.DDD
+{
+    public interface IDomainEvent:INotification
+    {
+        Guid EventId=>Guid.NewGuid();
+        public DateTime OccurredOn=>DateTime.Now;
+        public string EventType => GetType().AssemblyQualifiedName!;
+    }
+}
