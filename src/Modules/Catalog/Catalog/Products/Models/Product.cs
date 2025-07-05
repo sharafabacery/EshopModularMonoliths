4c7c9412c3266311e115560e43d55cfe44a96726
sharaf eldien ehab <52586357+sharafabacery@users.noreply.github.com>
2025-07-05 14:37:20 +0300

    Section 9: Develop Catalog Domain with Product Domain Entity Models

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,50 @@
+﻿using Catalog.Products.Events;
+
+namespace Catalog.Products.Models
+{
+    public class Product: Aggregate<Guid>
+    {
+        public string Name { get; private set; } = default!;
+        public List<string> Category {  get; private set; }= new List<string>();
+        public string Description {  get; private set; } = default!; 
+        public string ImageFile { get; private set; } = default!;
+        public decimal Price {  get; private set; } 
+
+        public static Product Create(Guid id,string name,List<string>category,string description,string imageFile,decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+            var product=new Product()
+            {
+                Id = id,
+                Name = name,
+                Category = category,
+                Description = description,
+                ImageFile = imageFile,
+                Price = price
+
+            };
+            product.AddDomainEvent(new ProductCreatedEvent(product));
+            return product;
+        }
+        public void Update(string name, List<string> category, string description, string imageFile, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+
+            this.Name = name;
+            this.Category = category;
+            this.Description = description;
+            this.ImageFile = imageFile;
+           
+            if (this.Price != price)
+            {
+                this.Price = price;
+                this.AddDomainEvent(new ProductPriceChangedEvent(this));
+
+            }
+
+        }
+
+    }
+}
--- src/Shared/Shared/DDD/IEntity.cs
+++ src/Shared/Shared/DDD/IEntity.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Shared.DDD
+﻿namespace Shared.DDD
 {
     public interface IEntity<T>: IEntity
     {
