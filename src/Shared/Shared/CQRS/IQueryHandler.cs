34313478f51917b91d1c978606ff5f827face792
Sharafeldin Ehab <sharafeldin.ehab@codezone-eg.com>
2025-07-08 11:17:47 +0300

    Section 12: Application Use Case Development with CQRS and MediatR

---
+++ src/Shared/Shared/CQRS/IQueryHandler.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+
+namespace Shared.CQRS
+{
+    public interface IQueryHandler<in IQuery, TResponse> : IRequestHandler<IQuery, TResponse>
+        where IQuery : IQuery<TResponse>
+        where TResponse : notnull
+    {
+    }
+}
